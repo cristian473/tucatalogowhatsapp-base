2920dc18ecbdc8263f0ecc618a0fa64e8dc3f27b
amorales-netbel <amorales@aulica.com.ar>
2025-06-19 11:49:48 -0300

    useEffect

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -70,6 +70,9 @@ const ProductDetail = () => {
     }
   });
   
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
   // Get product variants (same name, different presentations)
   useEffect(() => {
     const fetchProductVariants = async () => {
